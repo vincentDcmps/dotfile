04377bfcc84c5a061d4c5e6898b5c9f909778761
vincent <vincent@ducamps.win>
2018-11-11 18:55:47 +0100

    add shortcut for hide awesome wibo

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -284,6 +284,9 @@ root.buttons(gears.table.join(
 
 -- {{{ Key bindings
 globalkeys = gears.table.join(
+    awful.key({ modkey }, "b", function ()
+        mouse.screen.mywibox.visible = not mouse.screen.mywibox.visible
+        end,{description="toble wibox",group="awesome"}), 
     awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
               {description="show help", group="awesome"}),
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev,
