9dbe697d330a87dc35d1d9216052e793ca61c433
vincent <vincent@ducamps.win>
2019-02-11 20:12:46 +0100

    change polkit agent I3

--- script/script/install_env/i3.sh
+++ script/script/install_env/i3.sh
@@ -1,4 +1,4 @@
 
 pacman -Sy i3-gaps feh redshift
-pacman -SY light pamixer dunst rofi mpc arandr ttf-font-awesome-5
+pacman -SY light pamixer dunst rofi mpc arandr ttf-font-awesome-5 polkit-gnome
 yaourt aarchup polybar 
