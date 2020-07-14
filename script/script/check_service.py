37e0759d92fe80eaa32ef4a1b19d33ee51adc74a
vincent <vincent@ducamps.win>
2020-07-14 17:17:25 +0200

    remove print ard

--- script/script/check_service.py
+++ script/script/check_service.py
@@ -60,7 +60,6 @@ class Container(object):
 if __name__ == '__main__':
     wait=True
     if len(sys.argv) > 1:
-        print(sys.argv[1])
         if sys.argv[1] == "nowait":
             wait=False
     
