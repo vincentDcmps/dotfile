7466e264ad8289219578c1701468721057aa11d7
vincent <vincent@ducamps.win>
2022-01-28 15:35:27 +0100

    convert neo vim conf in lua

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,105 @@
+require('plugins')
+
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+-------------
+-- Options --
+-- ----------
+
+vim.opt.completeopt = {'menuone', 'noinsert', 'noselect'}  -- Completion options (for deoplete)
+vim.opt.ignorecase = true
+vim.opt.number = true
+vim.opt.smartcase = true
+vim.opt.mouse = 'a'
+vim.opt.background = 'dark'
+vim.opt.tabstop= 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth= 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.copyindent= true
+vim.opt.termguicolors = true
+vim.cmd([[colorscheme gruvbox]])
+
+
+---------------------
+--Global Varialbles--
+---------------------
+
+vim.g.airline_powerline_font = true
+vim.g.easyescape_chars = {
+  j = 1,
+  k = 1
+}
+vim.g.easyescape_timeout  = 100
+vim.g.python3_host_prog='/usr/bin/python3'
+vim.g.python_host_prog='/usr/bin/python'
+
+------------
+--Mappings--
+------------
+
+vim.g.mapleader= ' '
+
+-- Copy and paste with system clipboard
+
+vim.api.nvim_set_keymap('v', '<C-c>', '"+y',    {noremap = true})
+vim.api.nvim_set_keymap('i', '<C-v>', '<C-r>+', {noremap = true})
+
+vim.api.nvim_set_keymap('i', 'jk','<esc>', {noremap = true})
+vim.api.nvim_set_keymap('i', 'kj','<esc>', {noremap = true})
+
+vim.api.nvim_set_keymap('n', '<C-n>',':NvimTreeToggle<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>r',':NvimTreeRefresh<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>n',':NvimTreeFindFile<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<silent> <C-s>',':BufferPicleaderk<CR>', {noremap = true})
+vim.api.nvim_set_keymap('c', 'w!!','w !sudo tee > /dev/null %', {noremap = true})
+
+  local cmp = require'cmp'
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+       vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    mapping = {
+      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+      ['<C-e>'] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'ultisnips' }, -- For ultisnips users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline('/', {
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
---
+++ nvim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,148 @@
+
+-------------
+-- Plugins --
+-------------
+
+-- Auto install plugin manager
+
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require('packer').startup {
+  function(use)
+
+
+    use 'wbthomason/packer.nvim'
+
+    use {
+      'lewis6991/gitsigns.nvim',
+      requires = {
+        'nvim-lua/plenary.nvim'
+      },
+      config = function()
+        require('gitsigns').setup({
+        })
+      end
+    }
+
+   use 'ellisonleao/gruvbox.nvim'
+
+
+   use 'zhou13/vim-easyescape'
+
+
+   use {
+      'iamcco/markdown-preview.nvim',
+      run = function()
+        vim.fn['mkdp#util#install'](0)
+      end,
+      ft = {
+        'markdown'
+      },
+      config = function()
+        vim.api.nvim_set_keymap('n', '<LEADER>mp', ':MarkdownPreviewToggle<CR>', {noremap = true, silent = true})
+      end
+    }
+
+    -- Fuzzy search
+    use {
+      'junegunn/fzf.vim',
+      requires = {
+        'junegunn/fzf'
+      },
+      config = function()
+        vim.g.fzf_buffers_jump = 1
+        vim.api.nvim_set_keymap('n', '<LEADER><TAB>', ':Buffers!<CR>', {noremap = true})
+        vim.api.nvim_set_keymap('n', '<LEADER>ff', ':Files!<CR>\'', {noremap = true})
+        vim.api.nvim_set_keymap('n', '<LEADER>f.', ':Files! '..vim.fn.expand('%:p:h'), {noremap = true})
+        vim.api.nvim_set_keymap('n', '<LEADER>f/', ':Rg!<CR>', {noremap = true})
+        vim.api.nvim_set_keymap('n', '<LEADER>fg', ':GFiles!<CR>', {noremap = true})
+      end
+    }
+
+    -- Show indent line
+    use {
+      "lukas-reineke/indent-blankline.nvim",
+      config = function()
+        require("indent_blankline").setup {
+          char = "▏",
+          show_first_indent_level = false,
+          show_trailing_blankline_indent = false,
+        }
+      end
+    }
+  -- Autocomplete
+    use {
+      'hrsh7th/nvim-cmp',
+      requires = {
+        -- sources
+        'neovim/nvim-lspconfig',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-cmdline',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-path',
+
+        -- snippets
+        'SirVer/ultisnips',
+        -- pictograms
+        'onsails/lspkind-nvim'
+      }
+    }
+
+	-- File manager
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>NvimTreeToggle<cr>", {})
+			require("nvim-tree").setup({
+				auto_close = true,
+				update_focused_file = {
+					enable = true,
+				},
+				view = {
+					width = 40,
+				},
+			})
+		end,
+	})
+
+	use({
+		"folke/which-key.nvim",
+		config = function()
+			vim.api.nvim_set_option("timeoutlen", 300)
+			require("which-key").setup({})
+		end,
+	})
+  
+  use 'AckslD/nvim-whichkey-setup.lua'
+  use 'romgrk/barbar.nvim'
+  use 'famiu/feline.nvim'
+  use 'yamatsum/nvim-cursorline'
+  use 'freitass/todo.txt-vim'
+  
+  use 'tpope/vim-fugitive'
+
+  use ({
+    'windwp/nvim-autopairs',
+    config = function()
+      require('nvim-autopairs').setup{}
+    end
+  })
+
+  use({
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+  })
+
+  if packer_bootstrap then
+      require('packer').sync()
+    end
+  end 
+}
+
+
