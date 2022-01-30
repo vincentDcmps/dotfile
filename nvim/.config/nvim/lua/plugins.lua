52c2852aa92e31ead1ff5150b429646a3bf5acc3
vincent <vincent@ducamps.win>
2022-01-30 16:23:27 +0100

    add formatting on save

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -169,6 +169,16 @@ return require('packer').startup {
   use({'jose-elias-alvarez/null-ls.nvim',
         config= function()
           require("null-ls").setup({
+           on_attach = function(client)
+            if client.resolved_capabilities.document_formatting then
+              vim.cmd([[
+              augroup LspFormatting
+                  autocmd! * <buffer>
+                  autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+               augroup END
+              ]])
+            end
+          end,
         sources = {
           require("null-ls").builtins.formatting.prettier,
           require("null-ls").builtins.formatting.trim_whitespace,
