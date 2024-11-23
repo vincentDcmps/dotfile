c3650e3c2122bc562727e70af043a6513038802d
vincent <vincent@ducamps.win>
2024-11-23 14:35:39 +0100

    add neovim clipboard manager

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -40,6 +40,7 @@ vim.opt.scrolloff = 3 -- minimal number of screen lines to keep above and below 
 vim.opt.sidescrolloff = 3 -- minimal number of screen columns to keep to the left and right of the cursor if wrap is `false`
 vim.opt.shortmess:append("c") -- hide all the completion messages, e.g. "-- XXX completion (YYY)", "match 1 of 2", "The only match", "Pattern not found"
 vim.opt.whichwrap:append("<,>,[,],h,l") -- keys allowed to move to the previous/next line when the beginning/end of line is reached
+vim.opt.clipboard = "unnamedplus"
 
 
 
