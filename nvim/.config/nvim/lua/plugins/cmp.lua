5e72e215b5b04d2123c78f42f4420bcd5b81cc49
vincent <vincent@ducamps.win>
2025-08-10 19:32:04 +0200

    nvim: improve cmp

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,15 +1,49 @@
 return {
-    {
-    
-        "https://github.com/Saghen/blink.cmp",
-        version = "v1.*",
-        event = "VeryLazy",
-        config = function()
-            require("blink.cmp").setup({
-                keymap = {
-                    preset = "enter",
-                },
-            })
-        end,
+  {
+    "https://github.com/Saghen/blink.cmp",
+    version = "v1.*",
+    opts_extend = {
+      "sources.completion.enabled_providers",
+      "sources.compat",
+      "sources.default",
     },
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      -- add blink.compat to dependencies
+      {
+        "saghen/blink.compat",
+        optional = true, -- make optional so it's only enabled if any extras need it
+        opts = {},
+      }
+    },
+    opts = {
+      sources = {
+        -- adding any nvim-cmp sources here will enable them
+        -- with blink.compat
+        compat = {},
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+      keymap = {
+        preset = "super-tab",
+
+      },
+    },
+    event = "VeryLazy",
+    config = function(_, opts)
+      local enabled = opts.sources.default
+      for _, source in ipairs(opts.sources.compat or {}) do
+        opts.sources.providers[source] = vim.tbl_deep_extend(
+          "force",
+          { name = source, module = "blink.compat.source" },
+          opts.sources.providers[source] or {}
+        )
+        if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then
+          table.insert(enabled, source)
+        end
+      end
+      -- Unset custom prop to pass blink.cmp validation
+      opts.sources.compat = nil
+      require("blink.cmp").setup( opts )
+    end,
+  },
 }
